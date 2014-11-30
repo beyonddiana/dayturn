b8d3f435a5812333d70329ab7ad0d69390738ea4
Jonathan Yap <jhwelch@gmail.com>
2014-11-30 07:15:00 -0500

    From STORM-2082, wire up a bit more infrastructure for presets and deleting them.

--- indra/newview/llfloaterpreference.h
+++ indra/newview/llfloaterpreference.h
@@ -241,7 +241,10 @@ public:
 	// This function squirrels away the current values of the controls so that
 	// cancel() can restore them.
 	virtual void saveSettings();
-	
+
+	void onDeletePreset();
+	void onChangePreset();
+
 	class Updater;
 
 protected:
@@ -272,6 +275,9 @@ public:
 	void cancel();
 	void saveSettings();
 	void setHardwareDefaults();
+	void setPresetNamesInComboBox();
+	static const std::string getPresetsPath();
+
 protected:
 	bool hasDirtyChilds();
 	void resetDirtyChilds();
