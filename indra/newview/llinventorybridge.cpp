f83b7e1343265ad9e42f1b010293d8e9aa4c8b32
Jonathan Yap <jhwelch@gmail.com>
2015-01-06 13:21:38 -0500

    STORM-2091 A few small changes per code review requests.

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -3897,6 +3897,7 @@ void LLFolderBridge::buildContextMenuOptions(U32 flags, menuentry_vec_t&   items
 		LLInventoryModel::cat_array_t* cat_array;
 		LLInventoryModel::item_array_t* item_array;
 		gInventory.getDirectDescendentsOf(mUUID, cat_array, item_array);
+		// Enable Empty menu item only when there is something to act upon.
 		if (0 == cat_array->size() && 0 == item_array->size())
 		{
 			disabled_items.push_back(std::string("Empty Lost And Found"));
@@ -3950,6 +3951,7 @@ void LLFolderBridge::buildContextMenuOptions(U32 flags, menuentry_vec_t&   items
 		LLInventoryModel::cat_array_t* cat_array;
 		LLInventoryModel::item_array_t* item_array;
 		gInventory.getDirectDescendentsOf(mUUID, cat_array, item_array);
+		// Enable Empty menu item only when there is something to act upon.
 		if (0 == cat_array->size() && 0 == item_array->size())
 		{
 			disabled_items.push_back(std::string("Empty Trash"));
