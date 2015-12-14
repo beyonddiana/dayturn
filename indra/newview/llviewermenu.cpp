e922ab0b680e59c7328de8c50eadd85f334fdb15
Brad Payne (Vir Linden) <vir@lindenlab.com>
2015-12-14 11:48:23 -0500

    SL-278 WIP - Attachment points for new joints

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -200,10 +200,10 @@ LLMenuGL* gDetachSubMenu = NULL;
 LLMenuGL* gTakeOffClothes = NULL;
 LLContextMenu* gAttachScreenPieMenu = NULL;
 LLContextMenu* gAttachPieMenu = NULL;
-LLContextMenu* gAttachBodyPartPieMenus[8];
+LLContextMenu* gAttachBodyPartPieMenus[9];
 LLContextMenu* gDetachPieMenu = NULL;
 LLContextMenu* gDetachScreenPieMenu = NULL;
-LLContextMenu* gDetachBodyPartPieMenus[8];
+LLContextMenu* gDetachBodyPartPieMenus[9];
 
 //
 // Local prototypes
