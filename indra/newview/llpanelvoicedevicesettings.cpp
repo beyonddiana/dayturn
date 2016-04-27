b36d9d1217a462356debe6e8d6a09782a8b88345
Geir Nøklebye <geir.noklebye@dayturn.com>
2016-04-27 13:56:16 +0200

    VIRVOX: Mic setting changes some device list is up to date, mic loop test works, removed obsolete code and fine tuned voice state machine to avoid frequent neccessary code paths.
    Brett Joseph  committed 9e5afd0 on 2015-03-22

--- indra/newview/llpanelvoicedevicesettings.cpp
+++ indra/newview/llpanelvoicedevicesettings.cpp
@@ -51,7 +51,7 @@ LLPanelVoiceDeviceSettings::LLPanelVoiceDeviceSettings()
 	mCtrlOutputDevices = NULL;
 	mInputDevice = gSavedSettings.getString("VoiceInputAudioDevice");
 	mOutputDevice = gSavedSettings.getString("VoiceOutputAudioDevice");
-	mDevicesUpdated = FALSE;
+	mDevicesUpdated = FALSE;  //obsolete
 	mUseTuningMode = true;
 
 	// grab "live" mic volume level
@@ -80,6 +80,9 @@ BOOL LLPanelVoiceDeviceSettings::postBuild()
 	mLocalizedDeviceNames[DEFAULT_DEVICE]				= getString("default_text");
 	mLocalizedDeviceNames["No Device"]					= getString("name_no_device");
 	mLocalizedDeviceNames["Default System Device"]		= getString("name_default_system_device");
+
+	mCtrlOutputDevices->setMouseDownCallback(boost::bind(&LLPanelVoiceDeviceSettings::onOutputDevicesClicked, this));
+	mCtrlInputDevices->setMouseDownCallback(boost::bind(&LLPanelVoiceDeviceSettings::onInputDevicesClicked, this));
 	
 	return TRUE;
 }
@@ -226,9 +229,8 @@ void LLPanelVoiceDeviceSettings::refresh()
 			mCtrlOutputDevices->add(getLocalizedDeviceName(mOutputDevice), mOutputDevice, ADD_BOTTOM);
 			mCtrlOutputDevices->setValue(mOutputDevice);
 		}
-		mDevicesUpdated = FALSE;
 	}
-	else if (!mDevicesUpdated)
+	else if (LLVoiceClient::getInstance()->deviceSettingsUpdated())
 	{
 		LLVoiceDeviceList::const_iterator iter;
 		
@@ -272,7 +274,6 @@ void LLPanelVoiceDeviceSettings::refresh()
 				mOutputDevice = DEFAULT_DEVICE;
 			}
 		}
-		mDevicesUpdated = TRUE;
 	}	
 }
 
@@ -314,16 +315,26 @@ void LLPanelVoiceDeviceSettings::onCommitInputDevice()
 {
 	if(LLVoiceClient::getInstance())
 	{
-		LLVoiceClient::getInstance()->setCaptureDevice(
-			mCtrlInputDevices->getValue().asString());
+		mInputDevice = mCtrlInputDevices->getValue().asString();
+		LLVoiceClient::getInstance()->setRenderDevice(mInputDevice);
 	}
 }
 
 void LLPanelVoiceDeviceSettings::onCommitOutputDevice()
 {
 	if(LLVoiceClient::getInstance())
-	{
-		LLVoiceClient::getInstance()->setRenderDevice(
-			mCtrlInputDevices->getValue().asString());
+	{		
+		mOutputDevice = mCtrlOutputDevices->getValue().asString(); 
+		LLVoiceClient::getInstance()->setRenderDevice(mOutputDevice);
 	}
+}
+
+void LLPanelVoiceDeviceSettings::onOutputDevicesClicked()
+{
+	LLVoiceClient::getInstance()->refreshDeviceLists(false);  // fill in the pop up menus again if needed.
+}
+
+void LLPanelVoiceDeviceSettings::onInputDevicesClicked()
+{
+	LLVoiceClient::getInstance()->refreshDeviceLists(false);  // fill in the pop up menus again if needed.
 }
