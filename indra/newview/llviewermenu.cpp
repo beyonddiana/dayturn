0be3aa606fda326e6d31501b0ec64f7df508011b
Brad Payne (Vir Linden) <vir@lindenlab.com>
2016-05-09 10:25:30 -0400

    SL-315 - resetSkeleton() work + dae_tool.py can add random joint positions to models

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -6493,7 +6493,7 @@ class LLAvatarResetSkeleton: public view_listener_t
 		LLVOAvatar* avatar = find_avatar_from_object( LLSelectMgr::getInstance()->getSelection()->getPrimaryObject() );
 		if(avatar)
         {
-            avatar->resetSkeleton();
+            avatar->resetSkeleton(false);
         }
         return true;
     }
