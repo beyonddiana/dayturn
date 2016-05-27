4b756153c109fa1db17ba0b11901c75dbe5fb700
Geir Nøklebye <geir.noklebye@dayturn.com>
2016-05-27 20:59:49 +0200

    OS X: Add main-objc.mm. The application's entry point needs to be moved to main and not be dug into sub-project of the viewer. Not compiled yet as there are a number of other changes that must be made to make it work.

---
+++ indra/main-objc.mm
@@ -0,0 +1,15 @@
+//
+//  main-objc.mm
+//  Kokua
+//
+//  Created by Dayturn on 27.05.2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char * argv[]) {
+    return NSApplicationMain(argc, (const char**)argv);
+    
+}
