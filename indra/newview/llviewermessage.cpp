58932410878dd905c185fbf02627542385c13109
Mnikolenko Productengine <mnikolenko@productengine.com>
2016-06-14 13:47:27 +0300

    MAINT-5040 Warning "ll_apr_warn_status: APR: Connection refused" continually appears on Linux

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -5456,7 +5456,9 @@ void process_sound_trigger(LLMessageSystem *msg, void **)
 {
 	if (!gAudiop)
 	{
+#if !LL_LINUX
 		LL_WARNS("AudioEngine") << "LLAudioEngine instance doesn't exist!" << LL_ENDL;
+#endif
 		return;
 	}
 
@@ -5518,7 +5520,9 @@ void process_preload_sound(LLMessageSystem *msg, void **user_data)
 {
 	if (!gAudiop)
 	{
+#if !LL_LINUX
 		LL_WARNS("AudioEngine") << "LLAudioEngine instance doesn't exist!" << LL_ENDL;
+#endif
 		return;
 	}
 
