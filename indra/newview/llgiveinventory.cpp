4b059bac88707dcc1e38142fdb7cfe6a6b845fda
Mnikolenko Productengine <mnikolenko@productengine.com>
2016-06-22 17:53:47 +0300

    MAINT-1768 Allow sharing objects from trash folder

--- indra/newview/llgiveinventory.cpp
+++ indra/newview/llgiveinventory.cpp
@@ -249,7 +249,7 @@ bool LLGiveInventory::doGiveInventoryCategory(const LLUUID& to_agent,
 	gInventory.collectDescendentsIf (cat->getUUID(),
 		cats,
 		items,
-		LLInventoryModel::EXCLUDE_TRASH,
+		LLInventoryModel::INCLUDE_TRASH,
 		giveable);
 	S32 count = cats.size();
 	bool complete = true;
@@ -500,7 +500,7 @@ bool LLGiveInventory::commitGiveInventoryCategory(const LLUUID& to_agent,
 	gInventory.collectDescendentsIf (cat->getUUID(),
 		cats,
 		items,
-		LLInventoryModel::EXCLUDE_TRASH,
+		LLInventoryModel::INCLUDE_TRASH,
 		giveable);
 
 	bool give_successful = true;
