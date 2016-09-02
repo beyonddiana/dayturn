b8441d4b0fc940167b191e05387e1b9f7e3564a4
Geir Nøklebye <geir.noklebye@dayturn.com>
2016-09-02 12:45:44 +0200

    Partial MAINT-5011: Catch unhandled exceptions in LLCoros coroutines. (since we don't have all coroutines.) nat_linden  committed f33381d 2016-08-18

--- indra/llcommon/llexception.h
+++ indra/llcommon/llexception.h
@@ -31,6 +31,7 @@
  * boost::current_exception_diagnostic_information() is quite wonderful: if
  * all we need to do with an exception is log it, in most places we should
  * catch (...) and log boost::current_exception_diagnostic_information().
+ * See CRASH_ON_UNHANDLED_EXCEPTION() and LOG_UNHANDLED_EXCEPTION() below.
  *
  * There may be circumstances in which it would be valuable to distinguish an
  * exception explicitly thrown by viewer code from an exception thrown by
@@ -70,33 +71,15 @@ struct LLContinueError: public LLException
 // LLTHROW() in case we ever want to revisit that implementation decision.
 #define LLTHROW(x) BOOST_THROW_EXCEPTION(x)
 
-/**
- * Please use LLTHROW() to throw viewer exceptions whenever possible. This
- * enriches the exception's diagnostic_information() with the source file,
- * line and containing function of the LLTHROW() macro.
- */
-// Currently we implement that using BOOST_THROW_EXCEPTION(). Wrap it in
-// LLTHROW() in case we ever want to revisit that implementation decision.
-#define LLTHROW(x) BOOST_THROW_EXCEPTION(x)
-
-/**
- * Please use LLTHROW() to throw viewer exceptions whenever possible. This
- * enriches the exception's diagnostic_information() with the source file,
- * line and containing function of the LLTHROW() macro.
- */
-// Currently we implement that using BOOST_THROW_EXCEPTION(). Wrap it in
-// LLTHROW() in case we ever want to revisit that implementation decision.
-#define LLTHROW(x) BOOST_THROW_EXCEPTION(x)
-
 /// Call this macro from a catch (...) clause
-#define CRASH_ON_UNHANDLED_EXCEPTION() \
-     crash_on_unhandled_exception_(__FILE__, __LINE__, BOOST_CURRENT_FUNCTION)
-void crash_on_unhandled_exception_(const char*, int, const char*);
+#define CRASH_ON_UNHANDLED_EXCEPTION(CONTEXT) \
+     crash_on_unhandled_exception_(__FILE__, __LINE__, BOOST_CURRENT_FUNCTION, CONTEXT)
+void crash_on_unhandled_exception_(const char*, int, const char*, const std::string&);
 
 /// Call this from a catch (const LLContinueError&) clause, or from a catch
 /// (...) clause in which you do NOT want the viewer to crash.
-#define LOG_UNHANDLED_EXCEPTION() \
-     log_unhandled_exception_(__FILE__, __LINE__, BOOST_CURRENT_FUNCTION)
-void log_unhandled_exception_(const char*, int, const char*);
+#define LOG_UNHANDLED_EXCEPTION(CONTEXT) \
+     log_unhandled_exception_(__FILE__, __LINE__, BOOST_CURRENT_FUNCTION, CONTEXT)
+void log_unhandled_exception_(const char*, int, const char*, const std::string&);
 
-#endif /* ! defined(LL_LLEXCEPTION_H) */
+#endif /* ! defined(LL_LLEXCEPTION_H) */ 
