0ffa3cd0e5bcea133a47b20afb13b673b43ca751
Nicky Perian <nickyperian@yahoo.com>
2016-09-19 19:19:20 -0500

    Backed out changeset: 0bcc40a3554f Remove Trans button visibility from Texture picker.

--- indra/newview/lltexturectrl.cpp
+++ indra/newview/lltexturectrl.cpp
@@ -336,7 +336,7 @@ BOOL LLFloaterTexturePicker::postBuild()
 	childSetAction("Default",LLFloaterTexturePicker::onBtnSetToDefault,this);
 	childSetAction("None", LLFloaterTexturePicker::onBtnNone,this);
 	childSetAction("Blank", LLFloaterTexturePicker::onBtnBlank,this);
-//	childSetAction("Trans", LLFloaterTexturePicker::onBtnTrans,this);
+	childSetAction("Trans", LLFloaterTexturePicker::onBtnTrans,this);
 
 
 	childSetCommitCallback("show_folders_check", onShowFolders, this);
@@ -653,14 +653,12 @@ void LLFloaterTexturePicker::onBtnBlank(void* userdata)
 }
 
 // static
-/*
 void LLFloaterTexturePicker::onBtnTrans(void* userdata)
 {
 	LLFloaterTexturePicker* self = (LLFloaterTexturePicker*) userdata;
 	self->setImageID( self->mTransImageAssetID );
 	self->commitIfImmediateSet();
 }
-*/
 
 // static
 void LLFloaterTexturePicker::onBtnNone(void* userdata)
