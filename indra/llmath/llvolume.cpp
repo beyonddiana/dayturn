24f41a7dda065a27efe359c20b9f3200a20c8ad3
Brad Payne (Vir Linden) <vir@lindenlab.com>
2016-10-18 16:05:35 -0400

    MAINT-6841 - removed the joint remapping code, since it was designed to support a feature we no longer have. This also incidentally fixes any bugs caused by the joint remapping code.

--- indra/llmath/llvolume.cpp
+++ indra/llmath/llvolume.cpp
@@ -4612,7 +4612,6 @@ LLVolumeFace::LLVolumeFace() :
 	mTexCoords(NULL),
 	mIndices(NULL),
 	mWeights(NULL),
-    mWeightsRemapped(FALSE),
 	mOctree(NULL),
 	mOptimized(FALSE)
 {
@@ -4638,7 +4637,6 @@ LLVolumeFace::LLVolumeFace(const LLVolumeFace& src)
 	mTexCoords(NULL),
 	mIndices(NULL),
 	mWeights(NULL),
-    mWeightsRemapped(FALSE),
 	mOctree(NULL)
 { 
 	mExtents = (LLVector4a*) ll_aligned_malloc_16(sizeof(LLVector4a)*3);
@@ -4710,7 +4708,6 @@ LLVolumeFace& LLVolumeFace::operator=(const LLVolumeFace& src)
 			ll_aligned_free_16(mWeights);
 			mWeights = NULL;
 		}
-        mWeightsRemapped = src.mWeightsRemapped;
 	}
 
 	if (mNumIndices)
