a38795a89f96d7772737e762996ae70015fa0b37
Geir Nøklebye <geir.noklebye@dayturn.com>
2016-12-14 19:33:09 +0100

    Inspired by DRTVWR-418 change the darwin build directory to not have the architecture appended to the directory name. It is not very likely we need to have completely separate i386 and x86_64 builds. Usually one will build FAT binaries that contain both architecture in the same binary.

--- indra/newview/installers/darwin/fix_application_icon_position.sh
+++ indra/newview/installers/darwin/fix_application_icon_position.sh
@@ -1,12 +1,12 @@
 # just run this script each time after you change the installer's name to fix the icon misalignment 
 #!/bin/bash
-cp -r ../../../../build-darwin-i386/newview/*.dmg ~/Desktop/TempBuild.dmg
+cp -r ../../../../build-darwin/newview/*.dmg ~/Desktop/TempBuild.dmg
 hdid ~/Desktop/TempBuild.dmg
-open -a finder /Volumes/Second\ Life\ Installer
+open -a finder /Volumes/KokuaOS\ Installer
 osascript dmg-cleanup.applescript
-umount /Volumes/Second\ Life\ Installer/
+umount /Volumes/KokuaOS\ Installer/
 hdid ~/Desktop/TempBuild.dmg
-open -a finder /Volumes/Second\ Life\ Installer
+open -a finder /Volumes/KokuaOS\ Installer
 #cp /Volumes/Second\ Life\ Installer/.DS_Store ~/Desktop/_DS_Store
 #chflags nohidden ~/Desktop/_DS_Store
 #cp ~/Desktop/_DS_Store ./firstlook-dmg/_DS_Store
