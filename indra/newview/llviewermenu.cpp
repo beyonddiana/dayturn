1abec294dc9a81e45bf75469f2a49780c7f438d9
Nat Goodspeed <nat@lindenlab.com>
2016-12-21 11:33:58 -0500

    DRTVWR-418: Deal with fatal warning 'unsafe mix of bool and BOOL'

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -1286,7 +1286,7 @@ class LLAdvancedToggleWireframe : public view_listener_t
 		LLPipeline::updateRenderDeferred();
 		gPipeline.resetVertexBuffers();
 
-		if (!gUseWireframe && !gInitialDeferredModeForWireframe && LLPipeline::sRenderDeferred != gInitialDeferredModeForWireframe && gPipeline.isInit())
+		if (!gUseWireframe && !gInitialDeferredModeForWireframe && LLPipeline::sRenderDeferred != bool(gInitialDeferredModeForWireframe) && gPipeline.isInit())
 		{
 			LLPipeline::refreshCachedSettings();
 			gPipeline.releaseGLBuffers();
