a8fd6edd828646444f83a5a4b1f2af5dc561033a
Mnikolenko Productengine <mnikolenko@productengine.com>
2017-01-11 13:52:40 +0200

    MAINT-7031 /displayname & /completename should perform regardless of "View Display Names" viewer setting.

--- indra/llui/llurlentry.cpp
+++ indra/llui/llurlentry.cpp
@@ -764,7 +764,7 @@ LLUrlEntryAgentCompleteName::LLUrlEntryAgentCompleteName()
 
 std::string LLUrlEntryAgentCompleteName::getName(const LLAvatarName& avatar_name)
 {
-	return avatar_name.getCompleteName();
+	return avatar_name.getCompleteName(true, true);
 }
 
 //
@@ -796,7 +796,7 @@ LLUrlEntryAgentDisplayName::LLUrlEntryAgentDisplayName()
 
 std::string LLUrlEntryAgentDisplayName::getName(const LLAvatarName& avatar_name)
 {
-	return avatar_name.getDisplayName();
+	return avatar_name.getDisplayName(true);
 }
 
 //
