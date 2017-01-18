864c421036ce6b470145299f5ecee9c652ddf08e
Nicky Perian <nickyperian@yahoo.com>
2017-01-18 18:28:15 +0200

    MAINT-7061 Don't show the release notes floater for local builds Cherry picked from viewer-neko: Changeset:36893 (0d1bc6fe4862) MAINT-7061 Don't show the release notes floater for local builds  User: AndreyL ProductEngine While applying generalized to any channel with 'Test' in Channel.

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -2495,6 +2495,7 @@ void show_release_notes_if_required()
 void show_release_notes_if_required()
 {
     if (LLVersionInfo::getChannelAndVersion() != gLastRunVersion
+        && !(LLVersionInfo::getChannel().find("Test") != std::string::npos) // don't show Release Notes for the test builds
         && gSavedSettings.getBOOL("UpdaterShowReleaseNotes")
         && !gSavedSettings.getBOOL("FirstLoginThisInstall"))
     {
