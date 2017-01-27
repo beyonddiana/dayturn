4e6d7fd5c3413c3858123bef15acd4328387bea3
andreykproductengine <akleshchev@productengine.com>
2017-01-27 17:43:27 +0200

    MAINT-86 Fixed object-contents aren't updated after wearing from ground

--- indra/newview/llpanelobjectinventory.cpp
+++ indra/newview/llpanelobjectinventory.cpp
@@ -1901,6 +1901,7 @@ void LLPanelObjectInventory::refresh()
 			if(mTaskUUID != object->mID)
 			{
 				mTaskUUID = object->mID;
+				mAttachmentUUID = object->getAttachmentItemID();
 				make_request = TRUE;
 
 				// This is a new object so pre-emptively clear the contents
@@ -1909,6 +1910,16 @@ void LLPanelObjectInventory::refresh()
 
 				// Register for updates from this object,
 				registerVOInventoryListener(object,NULL);
+			}
+			else if (mAttachmentUUID != object->getAttachmentItemID())
+			{
+				mAttachmentUUID = object->getAttachmentItemID();
+				if (mAttachmentUUID.notNull())
+				{
+					// Server unsubsribes viewer (deselects object) from property
+					// updates after "ObjectAttach" so we need to resubscribe
+					LLSelectMgr::getInstance()->sendSelect();
+				}
 			}
 
 			// Based on the node information, we may need to dirty the
@@ -1940,6 +1951,7 @@ void LLPanelObjectInventory::refresh()
 void LLPanelObjectInventory::clearInventoryTask()
 {
 	mTaskUUID = LLUUID::null;
+	mAttachmentUUID = LLUUID::null;
 	removeVOInventoryListener();
 	clearContents();
 }
