ff31b801ee545c088c81b67d7a8b8e06ff8fc528
andreykproductengine <akleshchev@productengine.com>
2017-02-15 20:40:03 +0200

    MAINT-7118 Swapping legacy people API with new cache

--- indra/newview/llviewerinventory.h
+++ indra/newview/llviewerinventory.h
@@ -38,6 +38,8 @@ class LLInventoryPanel;
 class LLFolderView;
 class LLFolderBridge;
 class LLViewerInventoryCategory;
+class LLInventoryCallback;
+class LLAvatarName;
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Class LLViewerInventoryItem
@@ -157,7 +159,7 @@ public:
 	PermissionMask getPermissionMask() const;
 
 	// callback
-	void onCallingCardNameLookup(const LLUUID& id, const std::string& name, bool is_group);
+	void onCallingCardNameLookup(const LLUUID& id, const LLAvatarName& name);
 
 	// If this is a broken link, try to fix it and any other identical link.
 	BOOL regenerateLink();
