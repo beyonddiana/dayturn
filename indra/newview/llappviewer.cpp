bf671ea3c277ec0ae04e6d4db808f7d9679cb230
Oz Linden <oz@lindenlab.com>
2017-02-27 09:52:50 -0500

    Look for logcontrol-dev.xml in the user settings directory

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -2263,8 +2263,9 @@ void LLAppViewer::initLoggingAndGetLastDuration()
 	//
 	// Set up logging defaults for the viewer
 	//
-	LLError::initForApplication(
-				gDirUtilp->getExpandedFilename(LL_PATH_APP_SETTINGS, ""));
+	LLError::initForApplication( gDirUtilp->getExpandedFilename(LL_PATH_USER_SETTINGS, "")
+                                ,gDirUtilp->getExpandedFilename(LL_PATH_APP_SETTINGS, "")
+                                );
 	LLError::setFatalFunction(errorCallback);
 	//LLError::setTimeFunction(getRuntime);
 
