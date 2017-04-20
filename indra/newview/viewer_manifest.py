34e89020b18ef5fd28a2940851beb48611a035bc
Nicky Perian <nickyperian@yahoo.com>
2017-04-20 09:50:05 -0500

    Put libpangoft2.1 from Kokua repository back in play as system version delivered with Ubuntu-17.04 is not compatiable.

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -1394,6 +1394,7 @@ class Linux_i686_Manifest(LinuxManifest):
             self.path("libharfbuzz.so*")
             self.path("libpangocairo-1.0.so*")
             self.path("libpangoxft-1.0.so*")
+            self.path("libpangoft2-1.0.so*")
             self.path("libpixman-1.so*")
 
             # Include libfreetype.so. but have it work as libfontconfig does.
@@ -1512,6 +1513,7 @@ class Linux_x86_64_Manifest(LinuxManifest):
             self.path("libharfbuzz.so*")
             self.path("libpangocairo-1.0.so*")
             self.path("libpangoxft-1.0.so*")
+            self.path("libpangoft2-1.0.so*")
             self.path("libpixman-1.so*")
  
            #cef plugin
