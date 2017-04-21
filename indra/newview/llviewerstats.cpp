e0c26854c5e98232781cd1e3b832dfb3a28c6b2e
Nat Goodspeed <nat@lindenlab.com>
2017-04-21 15:27:10 -0400

    DRTVWR-418: Send address_size with login and viewer stats.

--- indra/newview/llviewerstats.cpp
+++ indra/newview/llviewerstats.cpp
@@ -511,6 +511,7 @@ void send_stats()
 	system["ram"] = (S32) gSysMemory.getPhysicalMemoryKB().value();
 	system["os"] = LLOSInfo::instance().getOSStringSimple();
 	system["cpu"] = gSysCPU.getCPUString();
+	system["address_size"] = ADDRESS_SIZE;
 	unsigned char MACAddress[MAC_ADDRESS_BYTES];
 	LLUUID::getNodeID(MACAddress);
 	std::string macAddressString = llformat("%02x-%02x-%02x-%02x-%02x-%02x",
