64e02ca2ea1390a99e26bb8d33f62920f14de437
Rider Linden <rider@lindenlab.com>
2017-07-28 14:07:25 -0700

    MAINT-7634: Move StatsAccumulator into llcommon, collect data sent and error codes from core.

--- indra/llcorehttp/httprequest.h
+++ indra/llcorehttp/httprequest.h
@@ -679,7 +679,20 @@ private:
 	/// @}
 	// End Global State
 	// ====================================
-	
+
+    struct Statistics
+    {
+        Statistics():
+            mTotalRequests(0),
+            mCurrentRequests(0)
+        {}
+
+        S32 mTotalRequests;
+        S32 mCurrentRequests;
+    };
+
+    static Statistics mStatistics;
+
 };  // end class HttpRequest
 
 
