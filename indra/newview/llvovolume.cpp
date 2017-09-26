d5c584538fc752d4db5e9fff947d2753ed7a93d8
Brad Payne (Vir Linden) <vir@lindenlab.com>
2017-09-26 15:39:22 +0100

    SL-800 - also need to add attachment overrides in the complementary case

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -3395,6 +3395,11 @@ void LLVOVolume::setExtendedMeshFlags(U32 flags)
                 // Making a rigged mesh into an animated object
                 getAvatarAncestor()->removeAttachmentOverridesForObject(this);
             }
+            else
+            {
+                // Making an animated object into a rigged mesh
+                getAvatarAncestor()->addAttachmentOverridesForObject(this);
+            }            
         }        
     }
 }
