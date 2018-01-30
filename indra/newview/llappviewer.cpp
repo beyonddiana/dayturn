bb009eb55f75633577e476a46a6e54eb7fde6bc6
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2018-01-30 14:03:26 +0000

    MAINT-8091 Removed deprecated and unused private memory pooling

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -812,7 +812,6 @@ bool LLAppViewer::init()
 	initMaxHeapSize() ;
 	LLCoros::instance().setStackSize(gSavedSettings.getS32("CoroutineStackSize"));
 
-	LLPrivateMemoryPoolManager::initClass((BOOL)gSavedSettings.getBOOL("MemoryPrivatePoolEnabled"), (U32)gSavedSettings.getU32("MemoryPrivatePoolSize")*1024*1024) ;
 	// write Google Breakpad minidump files to a per-run dump directory to avoid multiple viewer issues.
 	std::string logdir = gDirUtilp->getExpandedFilename(LL_PATH_DUMP, "");
 	mDumpPath = logdir;
@@ -1348,10 +1347,6 @@ bool LLAppViewer::mainLoop()
 	LLTimer debugTime;
 	
 	BOOL restore_rendering_masks = FALSE;
-
-	//LLPrivateMemoryPoolTester::getInstance()->run(false) ;
-	//LLPrivateMemoryPoolTester::getInstance()->run(true) ;
-	//LLPrivateMemoryPoolTester::destroy() ;
 
 	// Handle messages
 #ifdef LL_DARWIN
@@ -2161,9 +2156,6 @@ bool LLAppViewer::cleanup()
 	SUBSYSTEM_CLEANUP(LLWearableType);
 
 	LLMainLoopRepeater::instance().stop();
-
-	//release all private memory pools.
-	LLPrivateMemoryPoolManager::destroyClass() ;
 
 	ll_close_fail_log();
 
