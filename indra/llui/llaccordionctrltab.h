449b6534bfbbe11baa06a86ff8c6330caa2fef1e
Geir Nøklebye <geir.noklebye@dayturn.com>
2018-02-10 10:55:52 +0100

    Updating header files in preparation of taking MAINT-8274 FIXED. // TODO update the llaccordionctrl code to LL release version as there are issues with the current version in multiple places

--- indra/llui/llaccordionctrltab.h
+++ indra/llui/llaccordionctrltab.h
@@ -194,6 +194,8 @@ public:
 
 	void setFitPanel( bool fit ) { mFitPanel = true; }
 	bool getFitParent() const { return mFitPanel; }
+	
+	void setIgnoreResizeNotification(bool ignore) { mSkipChangesOnNotifyParent = ignore;}	
 
 protected:
 	void adjustContainerPanel	(const LLRect& child_rect);
@@ -235,6 +237,7 @@ private:
 
 	bool mStoredOpenCloseState;
 	bool mWasStateStored;
+	bool mSkipChangesOnNotifyParent;	
 
 	bool mSelectionEnabled;
 
