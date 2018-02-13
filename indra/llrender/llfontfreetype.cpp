5a07360191d5268d07428fc5791070b2828cef82
andreykproductengine <andreykproductengine@lindenlab.com>
2018-02-13 14:45:09 +0200

    MAINT-8146 Clearing pointers

--- indra/llrender/llfontfreetype.cpp
+++ indra/llrender/llfontfreetype.cpp
@@ -205,6 +205,7 @@ BOOL LLFontFreetype::loadFace(const std::string& filename, F32 point_size, F32 v
 	else
 	{
 		delete pFileStream;
+		pFileStream = NULL;
 		return FALSE;
 	}
 #else
@@ -220,6 +221,8 @@ BOOL LLFontFreetype::loadFace(const std::string& filename, F32 point_size, F32 v
 		pFileStream->close();
 		delete pFileStream;
 		delete pFtStream;
+		pFileStream = NULL;
+		pFtStream = NULL;
 #endif
 		return FALSE;
 	}
@@ -235,13 +238,15 @@ BOOL LLFontFreetype::loadFace(const std::string& filename, F32 point_size, F32 v
 
 	if (error)
 	{
+		// Clean up freetype libs.
+		FT_Done_Face(mFTFace);
 #ifdef LL_WINDOWS
 		pFileStream->close();
 		delete pFileStream;
 		delete pFtStream;
+		pFileStream = NULL;
+		pFtStream = NULL;
 #endif
-		// Clean up freetype libs.
-		FT_Done_Face(mFTFace);
 		mFTFace = NULL;
 		return FALSE;
 	}
