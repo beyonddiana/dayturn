9a8d28efc0400cb4ecf65b2b2427687ee9038fda
maxim_productengine <mnikolenko@productengine.com>
2018-02-26 18:03:59 +0200

    MAINT-8332 Handle hover height adjusting via debug setting

--- indra/newview/llfloaterhoverheight.cpp
+++ indra/newview/llfloaterhoverheight.cpp
@@ -44,7 +44,7 @@ LLFloaterHoverHeight::LLFloaterHoverHeight(const LLSD& key) : LLFloater(key)
 {
 }
 
-void LLFloaterHoverHeight::syncFromPreferenceSetting(void *user_data)
+void LLFloaterHoverHeight::syncFromPreferenceSetting(void *user_data, bool update_offset)
 {
 	F32 value = gSavedPerAccountSettings.getF32("AvatarHoverOffsetZ");
 
@@ -52,7 +52,13 @@ void LLFloaterHoverHeight::syncFromPreferenceSetting(void *user_data)
 	LLSliderCtrl* sldrCtrl = self->getChild<LLSliderCtrl>("HoverHeightSlider");
 	sldrCtrl->setValue(value,FALSE);
 
-
+	if (isAgentAvatarValid() && update_offset)
+ 	{
+ 		LLVector3 offset(0.0, 0.0, llclamp(value,MIN_HOVER_Z,MAX_HOVER_Z));
+ 		LL_INFOS("Avatar") << "setting hover from preference setting " << offset[2] << LL_ENDL;
+ 		gAgentAvatarp->setHoverOffset(offset);
+ 		//gAgentAvatarp->sendHoverHeight();
+ 	}
 }
 
 BOOL LLFloaterHoverHeight::postBuild()
@@ -69,7 +75,7 @@ BOOL LLFloaterHoverHeight::postBuild()
 	// Update slider on future pref changes.
 	if (gSavedPerAccountSettings.getControl("AvatarHoverOffsetZ"))
 	{
-		gSavedPerAccountSettings.getControl("AvatarHoverOffsetZ")->getCommitSignal()->connect(boost::bind(&syncFromPreferenceSetting, this));
+		gSavedPerAccountSettings.getControl("AvatarHoverOffsetZ")->getCommitSignal()->connect(boost::bind(&syncFromPreferenceSetting, this, false));
 	}
 	else
 	{
@@ -113,18 +119,14 @@ void LLFloaterHoverHeight::onSliderMoved(LLUICtrl* ctrl, void* userData)
 	}
 }
 
-// Do send-to-the-server work when slider drag completes, or new
-// value entered as text.
-void LLFloaterHoverHeight::onFinalCommit()
-{
-	LLSliderCtrl* sldrCtrl = getChild<LLSliderCtrl>("HoverHeightSlider");
-	F32 value = sldrCtrl->getValueF32();
-	LLVector3 offset(0.0, 0.0, llclamp(value,MIN_HOVER_Z,MAX_HOVER_Z));
-	gSavedPerAccountSettings.setF32("AvatarHoverOffsetZ",value);
-
-	LL_INFOS("Avatar") << "setting hover from slider final commit " << offset[2] << LL_ENDL;
-	gAgentAvatarp->setHoverOffset(offset, true); // will send update this time.
-}
+ // Do send-to-the-server work when slider drag completes, or new
+ // value entered as text.
+ void LLFloaterHoverHeight::onFinalCommit()
+ {
+ 	LLSliderCtrl* sldrCtrl = getChild<LLSliderCtrl>("HoverHeightSlider");
+ 	F32 value = sldrCtrl->getValueF32();
+ 	gSavedPerAccountSettings.setF32("AvatarHoverOffsetZ",value);
+ }
 
 void LLFloaterHoverHeight::onRegionChanged()
 {
--- indra/newview/llviewercontrol.cpp
+++ indra/newview/llviewercontrol.cpp
@@ -132,6 +132,16 @@ static bool handleDebugAvatarJointsChanged(const LLSD& newvalue)
     return true;
 }
 
+static bool handleAvatarHoverOffsetChanged(const LLSD& newvalue)
+{
+	if (isAgentAvatarValid())
+	{
+		gAgentAvatarp->setHoverIfRegionEnabled();
+	}
+	return true;
+}
+
+
 static bool handleSetShaderChanged(const LLSD& newvalue)
 {
 	// changing shader level may invalidate existing cached bump maps, as the shader type determines the format of the bump map it expects - clear and repopulate the bump cache
@@ -780,7 +790,7 @@ void settings_setup_listeners()
 	gSavedSettings.getControl("LoginLocation")->getSignal()->connect(boost::bind(&handleLoginLocationChanged));
     gSavedSettings.getControl("DebugAvatarJoints")->getCommitSignal()->connect(boost::bind(&handleDebugAvatarJointsChanged, _2));
     gSavedSettings.getControl("ViewerAssetHttpTypes")->getCommitSignal()->connect(boost::bind(&handleViewerAssetHttpTypesChanged, _2));
-
+	gSavedPerAccountSettings.getControl("AvatarHoverOffsetZ")->getCommitSignal()->connect(boost::bind(&handleAvatarHoverOffsetChanged, _2));
 }
 
 #if TEST_CACHED_CONTROL
