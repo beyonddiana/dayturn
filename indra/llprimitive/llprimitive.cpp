9e8dfc3147785952c50270d3f47fd1fe281d0adc
Anchor Linden <anchor@lindenlab.com>
2018-05-24 10:17:07 +0200

    [DRTVWR-455] - add new bake testure and tattoo slots

--- indra/llprimitive/llprimitive.cpp
+++ indra/llprimitive/llprimitive.cpp
@@ -1042,14 +1042,22 @@ S32 LLPrimitive::packTEField(U8 *cur_ptr, U8 *data_ptr, U8 data_size, U8 last_fa
 			}
 			
 			//assign exception faces to cur_ptr
-			if (exception_faces >= (0x1 << 7))
+			if (exception_faces >= ((U64)0x1 << 7))
 			{
-				if (exception_faces >= (0x1 << 14))
+				if (exception_faces >= ((U64)0x1 << 14))
 				{
-					if (exception_faces >= (0x1 << 21))
+					if (exception_faces >= ((U64)0x1 << 21))
 					{
-						if (exception_faces >= (0x1 << 28))
+						if (exception_faces >= ((U64)0x1 << 28))
 						{
+							if (exception_faces >= ((U64)0x1 << 35))
+							{
+								if (exception_faces >= ((U64)0x1 << 42))
+								{
+									*cur_ptr++ = (U8)(((exception_faces >> 42) & 0x7F) | 0x80);
+								}
+								*cur_ptr++ = (U8)(((exception_faces >> 35) & 0x7F) | 0x80);
+							}
 							*cur_ptr++ = (U8)(((exception_faces >> 28) & 0x7F) | 0x80);
 						}
 						*cur_ptr++ = (U8)(((exception_faces >> 21) & 0x7F) | 0x80);
@@ -1058,6 +1066,7 @@ S32 LLPrimitive::packTEField(U8 *cur_ptr, U8 *data_ptr, U8 data_size, U8 last_fa
 				}
 				*cur_ptr++ = (U8)(((exception_faces >> 7) & 0x7F) | 0x80);
 			}
+
 			
 			*cur_ptr++ = (U8)(exception_faces & 0x7F);
 			
@@ -1117,7 +1126,7 @@ S32 LLPrimitive::unpackTEField(U8 *cur_ptr, U8 *buffer_end, U8 *data_ptr, U8 dat
 // Includes information about image ID, color, scale S,T, offset S,T and rotation
 bool LLPrimitive::packTEMessage(LLMessageSystem *mesgsys) const
 {
-	const U32 MAX_TES = 32;
+	const U32 MAX_TES = 42;
 
 	U8     image_ids[MAX_TES*16];
 	U8     colors[MAX_TES*4];
@@ -1202,7 +1211,7 @@ bool LLPrimitive::packTEMessage(LLMessageSystem *mesgsys) const
 
 bool LLPrimitive::packTEMessage(LLDataPacker &dp) const
 {
-	const U32 MAX_TES = 32;
+	const U32 MAX_TES = 42;
 
 	U8     image_ids[MAX_TES*16];
 	U8     colors[MAX_TES*4];
@@ -1315,6 +1324,8 @@ S32 LLPrimitive::parseTEMessage(LLMessageSystem* mesgsys, char const* block_name
 		mesgsys->getBinaryDataFast(block_name, _PREHASH_TextureEntry, tec.packed_buffer, 0, block_num, LLTEContents::MAX_TE_BUFFER);
 	}
 
+	
+
 	tec.face_count = llmin((U32)getNumTEs(),(U32)LLTEContents::MAX_TES);
 
 	U8 *cur_ptr = tec.packed_buffer;
@@ -1387,6 +1398,8 @@ S32 LLPrimitive::applyParsedTEMessage(LLTEContents& tec)
 
 		retval |= setTEColor(i, color);
 
+		
+
 	}
 
 	return retval;
@@ -1405,7 +1418,7 @@ S32 LLPrimitive::unpackTEMessage(LLDataPacker &dp)
 {
 	// use a negative block_num to indicate a single-block read (a non-variable block)
 	S32 retval = 0;
-	const U32 MAX_TES = 32;
+	const U32 MAX_TES = 42;
 
 	// Avoid construction of 32 UUIDs per call
 	static LLUUID image_ids[MAX_TES];
