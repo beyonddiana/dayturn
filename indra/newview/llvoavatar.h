cc901af389f8b770212e9528cd98fba998320d3d
Anchor Linden <anchor@lindenlab.com>
2018-06-26 22:15:01 +0200

    [MAINT-8513] - fix "Edit My Outfit" Changes Don't Update

--- indra/newview/llvoavatar.h
+++ indra/newview/llvoavatar.h
@@ -696,6 +696,7 @@ public:
 	virtual void	dirtyMesh(); // Dirty the avatar mesh
 	void 			updateMeshData();
 	void			updateMeshVisibility();
+	LLViewerTexture*		getBakedTexture(const U8 te);
 
 protected:
 	void 			releaseMeshData();
