f9509dda6d7d0066c26a02bd1237307f5f44d552
andreykproductengine <andreykproductengine@lindenlab.com>
2018-11-15 15:14:00 +0200

    SL-9782 Fixed The Stand button doesn't lay anymore on the lower bar

--- indra/newview/llmoveview.h
+++ indra/newview/llmoveview.h
@@ -179,8 +179,6 @@ private:
 	 */
 	LLHandle<LLPanel> mOriginalParent;
 
-	LLHandle<LLPanel> mStateManagementButtons;
-
 	/**
 	 * True if the panel is currently attached to the movement controls floater.
 	 * 
