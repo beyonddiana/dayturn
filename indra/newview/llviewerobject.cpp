3538108fc5aadca36dea23feade1440a2f4bccbb
Brad Payne (Vir Linden) <vir@lindenlab.com>
2019-01-08 21:28:47 +0000

    SL-10285 - removed one possible route for the isImpostor() crash. Intermittent issue.

--- indra/newview/llviewerobject.cpp
+++ indra/newview/llviewerobject.cpp
@@ -3111,6 +3111,7 @@ void LLViewerObject::unlinkControlAvatar()
         if (mControlAvatar)
         {
             mControlAvatar->markForDeath();
+			mControlAvatar->mRootVolp = NULL;
             mControlAvatar = NULL;
         }
     }
