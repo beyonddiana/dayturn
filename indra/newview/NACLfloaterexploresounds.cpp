3d8500839b3eb3da828984f4de5d9e34bb750847
Geir Nøklebye <geir.noklebye@dayturn.com>
2019-02-12 14:33:00 +0100

    Convert tick() in NACLfloaterexploresounds to bool

--- indra/newview/NACLfloaterexploresounds.cpp
+++ indra/newview/NACLfloaterexploresounds.cpp
@@ -146,7 +146,7 @@ public:
 	}
 };
 
-BOOL NACLFloaterExploreSounds::tick()
+bool NACLFloaterExploreSounds::tick()
 {
 	static const std::string str_playing =  getString("Playing");
 	static const std::string str_not_playing = getString("NotPlaying");
