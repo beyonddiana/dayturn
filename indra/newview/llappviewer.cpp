71a71300aaab967e8b6771999bc258d7cc05fced
Geir Nøklebye <geir.noklebye@dayturn.com>
2019-02-17 11:43:51 +0100

    Update the Release Notes base URL and turn off showing release notes on updates, as we currently don't have any updater function for the viewer. Presumably the user will already have read the release notes when they manually downloaded and updated the viewer.  The Release Notes URL in the About floater will point to the Resources section of the viewer forum where release are listed latest to oldest.

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -3164,13 +3164,13 @@ LLSD LLAppViewer::getViewerInfo() const
 	std::string url = LLTrans::getString("RELEASE_NOTES_BASE_URL");
 	if (! LLStringUtil::endsWith(url, "/"))
 		url += "/";
-	std::string channel = LLVersionInfo::getChannel();
-	if (LLStringUtil::endsWith(boost::to_lower_copy(channel), " edu")) // Release Notes url shouldn't include the EDU parameter
-	{
-		boost::erase_tail(channel, 4);
-	}
-	url += LLURI::escape(channel) + "/";
-	url += LLURI::escape(LLVersionInfo::getVersion());
+//	std::string channel = LLVersionInfo::getChannel();
+// 	if (LLStringUtil::endsWith(boost::to_lower_copy(channel), " edu")) // Release Notes url shouldn't include the EDU parameter
+// 	{
+// 		boost::erase_tail(channel, 4);
+// 	}
+// 	url += LLURI::escape(channel) + "/";
+//	url += LLURI::escape(LLVersionInfo::getVersion());
 
 	info["VIEWER_RELEASE_NOTES_URL"] = url;
 #if LL_MSVC
