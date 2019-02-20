a4d7d964968661f6b32cf0e0cb3fa33f3028dd35
Marine Kelley <none@none>
2019-02-21 00:12:43 +0100

    Change the way we render alpha surfaces when the vision is restricted : do it at the face level when it is about to be rendered, instead of once in a while when the geometry is rebuilt. => alpha surfaces are not rendered when they are too far or if they are invisible => this also fixes the problem of invisible surfaces not showing in red when highlighting invisible prims until we pressed Ctrl Alt V

--- indra/newview/lltoolfocus.cpp
+++ indra/newview/lltoolfocus.cpp
@@ -232,6 +232,13 @@ void LLToolCamera::pickCallback(const LLPickInfo& pick_info)
 			if (!hit_obj->isHUDAttachment())
 			{
 				gAgentCamera.setFocusOnAvatar(FALSE, ANIMATE);
+//MK
+				if (pick_info.mKeyMask & MASK_SHIFT)
+				{
+
+				}
+				else
+//mk
 				gAgentCamera.setFocusGlobal(pick_info);
 			}
 		}
@@ -239,6 +246,13 @@ void LLToolCamera::pickCallback(const LLPickInfo& pick_info)
 		{
 			// Hit the ground
 			gAgentCamera.setFocusOnAvatar(FALSE, ANIMATE);
+//MK
+			if (pick_info.mKeyMask & MASK_SHIFT)
+			{
+
+			}
+			else
+//mk
 			gAgentCamera.setFocusGlobal(pick_info);
 		}
 
