1917969a8c427f3131ce115687f597a41689bf15
Geir Nøklebye <geir.noklebye@dayturn.com>
2019-04-30 15:46:11 +0200

    Updates from upstream Firestorm to aoset

--- indra/newview/aoset.h
+++ indra/newview/aoset.h
@@ -19,12 +19,12 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * $/LicenseInfo$
  */
 
 #ifndef AOSET_H
 #define AOSET_H
 
-#include "llcommon.h"
 #include "lleventtimer.h"
 
 class AOSet
