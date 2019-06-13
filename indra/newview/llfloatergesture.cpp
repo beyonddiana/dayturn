7bcfb2c2d4782d5a7d1b06d27e677f9a9192eb18
Geir Nøklebye <geir.noklebye@dayturn.com>
2019-06-13 14:46:24 +0200

    DAYTW-32 Convert BOOL in llstring

--- indra/newview/llfloatergesture.cpp
+++ indra/newview/llfloatergesture.cpp
@@ -50,7 +50,7 @@
 #include "llviewercontrol.h"
 #include "llfloaterperms.h"
 
-BOOL item_name_precedes( LLInventoryItem* a, LLInventoryItem* b )
+bool item_name_precedes( LLInventoryItem* a, LLInventoryItem* b )
 {
 	return LLStringUtil::precedesDict( a->getName(), b->getName() );
 }
