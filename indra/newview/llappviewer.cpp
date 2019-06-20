c78cc55876f39d42bb2f2acbc6d0c30492cee5b9
andreykproductengine <andreykproductengine@lindenlab.com>
2019-06-20 20:53:46 +0300

    DRTVWR-493 LLLocalBitmapMgr to Singleton

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1884,8 +1884,6 @@ bool LLAppViewer::cleanup()
 	gTransferManager.cleanup();
 #endif
 
-	SUBSYSTEM_CLEANUP(LLLocalBitmapMgr);
-
 	// Note: this is where gWorldMap used to be deleted.
 
 	// Note: this is where gHUDManager used to be deleted.
