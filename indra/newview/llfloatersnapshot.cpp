973ff6b1653ad6fb56606f1713242f7c29253c6d
ruslantproductengine <ruslantproductengine@lindenlab.com>
2019-07-11 20:30:30 +0300

    SL-11435 When ALM is enabled, Depth mode shots are broken when snapshot size is set to anything above current window size - fixed bug described in the ticket - fixed bug with UI (when user change the layer type (color/depth))

--- indra/newview/llfloatersnapshot.cpp
+++ indra/newview/llfloatersnapshot.cpp
@@ -929,7 +929,7 @@ void LLFloaterSnapshot::Impl::onCommitLayerTypes(LLUICtrl* ctrl, void*data)
 		{
 			previewp->setSnapshotBufferType((LLViewerWindow::ESnapshotType)combobox->getCurrentIndex());
 		}
-		checkAutoSnapshot(previewp, TRUE);
+		previewp->updateSnapshot(TRUE, TRUE);
 	}
 }
 
