4b432ad0352ac192c3f81b37157621755b318b64
andreykproductengine <andreykproductengine@lindenlab.com>
2019-08-15 19:53:34 +0300

    SL-209 Cleanup Merchant Outbox

--- indra/newview/lltooldraganddrop.cpp
+++ indra/newview/lltooldraganddrop.cpp
@@ -1897,6 +1897,7 @@ EAcceptance LLToolDragAndDrop::dad3dRezAttachmentFromInv(
 	const LLUUID &outbox_id = gInventory.findCategoryUUIDForType(LLFolderType::FT_OUTBOX, false);
 	if(gInventory.isObjectDescendentOf(item->getUUID(), outbox_id))
 	{
+		// Legacy
 		return ACCEPT_NO;
 	}
 
@@ -2362,6 +2363,7 @@ EAcceptance LLToolDragAndDrop::dad3dWearCategory(
 		const LLUUID &outbox_id = gInventory.findCategoryUUIDForType(LLFolderType::FT_OUTBOX, false);
 		if(gInventory.isObjectDescendentOf(category->getUUID(), outbox_id))
 		{
+			// Legacy
 			return ACCEPT_NO;
 		}
 
