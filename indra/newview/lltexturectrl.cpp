1eb0a819e62615bc08dcdd09a16f6fcdf3985cfe
ruslantproductengine <ruslantproductengine@lindenlab.com>
2019-10-18 22:38:33 +0300

    SL-11840 [Bakes on Mesh] Enable bake textures to be applied to non-attached editable items - Now bake texture channel's available for assign for non attached objects

--- indra/newview/lltexturectrl.cpp
+++ indra/newview/lltexturectrl.cpp
@@ -440,7 +440,7 @@ bool LLFloaterTexturePicker::postBuild()
 	getChild<LLComboBox>("l_bake_use_texture_combo_box")->setCommitCallback(onBakeTextureSelect, this);
 	getChild<LLCheckBoxCtrl>("hide_base_mesh_region")->setCommitCallback(onHideBaseMeshRegionCheck, this);
 	
-	setBakeTextureEnabled(FALSE);
+	setBakeTextureEnabled(TRUE);
 	return true;
 }
 
@@ -1225,8 +1225,7 @@ LLTextureCtrl::LLTextureCtrl(const LLTextureCtrl::Params& p)
 	mImageAssetID(p.image_id),
 	mDefaultImageAssetID(p.default_image_id),
 	mDefaultImageName(p.default_image_name),
-	mFallbackImage(p.fallback_image),
-	mBakeTextureEnabled(FALSE)
+	mFallbackImage(p.fallback_image)
 {
 
 	// Default of defaults is white image for diff tex
@@ -1419,7 +1418,7 @@ void LLTextureCtrl::showPicker(BOOL take_focus)
 		}
 		if (texture_floaterp)
 		{
-			texture_floaterp->setBakeTextureEnabled(mBakeTextureEnabled);
+			texture_floaterp->setBakeTextureEnabled(TRUE);
 		}
 
 		LLFloater* root_floater = gFloaterView->getParentFloater(this);
@@ -1598,7 +1597,6 @@ void LLTextureCtrl::setImageAssetID( const LLUUID& asset_id )
 
 void LLTextureCtrl::setBakeTextureEnabled(BOOL enabled)
 {
-	mBakeTextureEnabled = enabled;
 	LLFloaterTexturePicker* floaterp = (LLFloaterTexturePicker*)mFloaterHandle.get();
 	if (floaterp)
 	{
