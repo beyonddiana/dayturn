cb08f389bb6b59564f344ecc46dc6500b2505063
andreykproductengine <andreykproductengine@lindenlab.com>
2019-10-23 20:27:05 +0300

    Actually Windows build fix

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -63,7 +63,9 @@
 #include "llcurl.h"
 #include "llcalc.h"
 #include "llconversationlog.h"
+#if LL_WINDOWS
 #include "lldxhardware.h"
+#endif
 #include "lltexturestats.h"
 #include "lltrace.h"
 #include "lltracethreadrecorder.h"
