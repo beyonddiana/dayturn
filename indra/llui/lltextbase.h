9314d397eb644fb5f1f3f3a18d896c4d80472d55
maxim_productengine <mnikolenko@productengine.com>
2019-11-22 18:26:56 +0200

    SL-12317 UI Changes to Group limits GN: Minus the SL premium plus part of the change

--- indra/llui/lltextbase.h
+++ indra/llui/lltextbase.h
@@ -310,6 +310,7 @@ public:
 								border_visible,
 								track_end,
 								read_only,
+								skip_link_underline,
 								spellcheck,
 								allow_scroll,
 								plain_text,
@@ -458,6 +459,9 @@ public:
 
 	void					setReadOnly(bool read_only) { mReadOnly = read_only; }
 	bool					getReadOnly() { return mReadOnly; }
+
+	void					setSkipLinkUnderline(bool skip_link_underline) { mSkipLinkUnderline = skip_link_underline; }
+	bool					getSkipLinkUnderline() { return mSkipLinkUnderline;  }
 
 	void					setPlainText(bool value) { mPlainText = value;}
 	bool					getPlainText() const { return mPlainText; }
@@ -703,6 +707,8 @@ protected:
 	bool						mAutoIndent;
 	S32							mMaxTextByteLength;	// Maximum length mText is allowed to be in bytes
 	
+	bool						mSkipLinkUnderline;
+
 	bool						mSkipTripleClick;
 
 	// support widgets
