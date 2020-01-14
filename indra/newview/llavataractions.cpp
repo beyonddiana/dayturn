7fc5f1bb697a761e635d0b2265291dc52b175ec8
Geir Nøklebye <geir.noklebye@dayturn.com>
2020-01-14 10:29:30 +0100

    Build fix for llavataractions

--- indra/newview/llavataractions.cpp
+++ indra/newview/llavataractions.cpp
@@ -1839,5 +1839,5 @@ void LLAvatarActions::estateBanCommit(const LLSD &notification, const LLSD &resp
 	}
 
 	LLFloaterRegionInfo::nextInvoice();
-	LLPanelEstateInfo::sendEstateAccessDelta(flags, avatar->getID());
+	LLPanelEstateAccess::sendEstateAccessDelta(flags, avatar->getID());
 }
