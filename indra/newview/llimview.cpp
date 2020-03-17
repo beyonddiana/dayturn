1ceb7e3fdf6f2acdb278c6ea7c754d7871f279ad
Mnikolenko Productengine <mnikolenko@productengine.com>
2020-03-17 16:55:10 +0200

    SL-12860 FIXED Enabling 'Only Friends and Groups can call or IM me' still plays New Conversation sound

--- indra/newview/llimview.cpp
+++ indra/newview/llimview.cpp
@@ -2797,7 +2797,7 @@ void LLIMMgr::addMessage(
 			}
 
 			//Play sound for new conversations
-			if (!gAgent.isDoNotDisturb() && (gSavedSettings.getBOOL("PlaySoundNewConversation") == TRUE))
+			if (!skip_message & !gAgent.isDoNotDisturb() && (gSavedSettings.getBOOL("PlaySoundNewConversation") == TRUE))
 			{
 				make_ui_sound("UISndNewIncomingIMSession");
 			}
