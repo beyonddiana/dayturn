bcfb9ec546f08090944446a46d84b4358beee98d
Runitai Linden <davep@lindenlab.com>
2020-03-26 16:48:33 -0500

    SL-12902 Fix for doing the technically correct but compatibility wrong thing WRT light color values.
    # Conflicts:
    #	indra/newview/llvovolume.h

--- indra/newview/pipeline.cpp
+++ indra/newview/pipeline.cpp
@@ -6371,6 +6371,7 @@ void LLPipeline::setupHWLights(LLDrawPool* pool)
 				mLightMovingMask |= (1<<cur_light);
 			}
 			
+            //NOTE: for legacy reasons, send sRGB color to light shader for both deferred and non-deferred path
 			LLColor4  light_color = light->getLightColor();
 			light_color.mV[3] = 0.0f;
 
@@ -8713,7 +8714,8 @@ void LLPipeline::renderDeferredLighting()
 					const F32* c = center.getF32ptr();
 					F32 s = volume->getLightRadius()*1.5f;
 
-					LLColor3 col = volume->getLightColor();
+                    //NOTE: for legacy reasons, send sRGB color to light shader
+                    LLColor3 col = volume->getLightColor();
 
                     // fade also works as flicker prevention during reparenting
                     // because reparenting causes distance to jump temporary
@@ -8831,7 +8833,8 @@ void LLPipeline::renderDeferredLighting()
 
 					setupSpotLight(gDeferredSpotLightProgram, drawablep);
 					
-					LLColor3 col = volume->getLightColor();
+                    //NOTE: for legacy reasons, send sRGB color to light shader
+                    LLColor3 col = volume->getLightColor();
 					/*col.mV[0] = powf(col.mV[0], 2.2f);
 					col.mV[1] = powf(col.mV[1], 2.2f);
 					col.mV[2] = powf(col.mV[2], 2.2f);*/
@@ -8927,7 +8930,8 @@ void LLPipeline::renderDeferredLighting()
 					
 					setupSpotLight(gDeferredMultiSpotLightProgram, drawablep);
 
-					LLColor3 col = volume->getLightColor();
+                    //NOTE: for legacy reasons, send sRGB color to light shader
+                    LLColor3 col = volume->getLightColor();
 					
 					/*col.mV[0] = powf(col.mV[0], 2.2f);
 					col.mV[1] = powf(col.mV[1], 2.2f);
