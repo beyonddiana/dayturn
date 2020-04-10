f1345ba6b055e5d3daa71c703b28465c88bbea71
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-04-10 13:58:58 +0300

    SL-11445 Provide 'name' for Pulse Audio

--- indra/llaudio/llaudioengine.cpp
+++ indra/llaudio/llaudioengine.cpp
@@ -114,7 +114,7 @@ void LLAudioEngine::setDefaults()
 }
 
 
-bool LLAudioEngine::init(const S32 num_channels, void* userdata)
+bool LLAudioEngine::init(const S32 num_channels, void* userdata, const std::string &app_title)
 {
 	setDefaults();
 
