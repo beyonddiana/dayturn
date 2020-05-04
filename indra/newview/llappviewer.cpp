a46c8dfae55b479b64ea3ea9501ab89fc0dfe89b
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-05-05 01:15:25 +0300

    SL-12527 cursor is not changed to the pointer on the MoTD link when teleporting

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -4782,12 +4782,13 @@ void LLAppViewer::idle()
     {
 		return;
     }
+
+    gViewerWindow->updateUI();
+
 	if (gTeleportDisplay)
     {
 		return;
     }
-
-	gViewerWindow->updateUI();
 
 	///////////////////////////////////////
 	// Agent and camera movement
