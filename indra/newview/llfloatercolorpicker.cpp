d2ad983e6ca3dd1436a30b8297b2f88e4741640a
Geir Nøklebye <geir.noklebye@dayturn.com>
2020-05-07 17:41:48 +0200

    DAYTW-32 Make active in LLFloaterColorPicker::setActive a bool for consistency

--- indra/newview/llfloatercolorpicker.cpp
+++ indra/newview/llfloatercolorpicker.cpp
@@ -1117,7 +1117,7 @@ void LLFloaterColorPicker::setMouseDownInSwatch (BOOL mouse_down_in_swatch)
 	}
 }
 
-void LLFloaterColorPicker::setActive(BOOL active) 
+void LLFloaterColorPicker::setActive(bool active) 
 { 
 	// shut down pipette tool if active
 	if (!active && mPipetteBtn->getToggleState())
