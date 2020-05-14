e9159d6e25f38bca1f67d7d14a7d4d1125c8044d
Andrey Lihatskiy <andreylproductengine@lindenlab.com>
2020-05-14 07:51:08 +0000

    Merged in SL-12090 (pull request #105)
    SL-12090 Initialize the joystick only when required
    
    * SL-12090 Initialize the joystick only when required
    
    Approved-by: Andrey Kleshchev

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1132,7 +1132,10 @@ bool LLAppViewer::init()
 	gSimLastTime = gRenderStartTime.getElapsedTimeF32();
 	gSimFrames = (F32)gFrameCount;
 
-	LLViewerJoystick::getInstance()->init(false);
+    if (gSavedSettings.getBOOL("JoystickEnabled"))
+    {
+        LLViewerJoystick::getInstance()->init(false);
+    }
 
 	try {
 		initializeSecHandler();
