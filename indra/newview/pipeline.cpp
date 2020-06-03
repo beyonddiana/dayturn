b36c68b917475136d7df018d186eee4730320b05
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-06-03 23:36:28 +0300

    SL-863 Fixed missing render mask

--- indra/newview/pipeline.cpp
+++ indra/newview/pipeline.cpp
@@ -11385,7 +11385,9 @@ void LLPipeline::generateImpostor(LLVOAvatar* avatar)
 	
 	if (visually_muted)
 	{
-		andRenderTypeMask(LLPipeline::RENDER_TYPE_AVATAR, END_RENDER_TYPES);
+		andRenderTypeMask(LLPipeline::RENDER_TYPE_AVATAR,
+							LLPipeline::RENDER_TYPE_CONTROL_AV,
+							END_RENDER_TYPES);
 	}
 	else
 	{
