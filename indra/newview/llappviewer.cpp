0d0eec2dd55c38a2e79c3485a9948c9c51d464b3
Dave Houlton <euclid@lindenlab.com>
2020-08-20 15:08:47 -0600

    SL-13565, remove SL-10625 throttling erroneously applied to all GPUs
    Remove the sleep throttle applied in 10625 to work around an Intel GPU driver
    bug. It was erroneously being applied to all GPUs, and also as of Intel driver
    26.20.100.7755 (1/9/2020) it appears that the driver bug no longer exists.

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1554,23 +1554,8 @@ bool LLAppViewer::mainLoop()
 					RRInterface::sRenderLimitRenderedThisFrame = FALSE;
 //mk
 
-                    static U64 last_call = 0;
-                    if (!gTeleportDisplay)
-                    {
-                        // Frame/draw throttling
-                        U64 elapsed_time = LLTimer::getTotalTime() - last_call;
-                        if (elapsed_time < mMinMicroSecPerFrame)
-                        {
-                            LL_RECORD_BLOCK_TIME(FTM_SLEEP);
-                            // llclamp for when time function gets funky
-                            U64 sleep_time = llclamp(mMinMicroSecPerFrame - elapsed_time, (U64)1, (U64)1e6);
-                            micro_sleep(sleep_time, 0);
-                        }
-                    }
-                    last_call = LLTimer::getTotalTime();
-				
-					display();
-					
+ 					display();
+                    
 					pingMainloopTimeout("Main:Snapshot");
 					LLFloaterSnapshot::update(); // take snapshots
                     //TODO: Make one call by moving LLFloaterOutfitSnapshot::update() to LLFloaterSnapshotBase class
