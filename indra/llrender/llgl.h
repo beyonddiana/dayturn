b8241f5cd7d1457e140b78df9ac347e8f7557e26
Dave Houlton <euclid@lindenlab.com>
2020-10-16 17:10:49 -0600

    DRTVWR-510 remove obsolete mDebugGPU variable

--- indra/llrender/llgl.h
+++ indra/llrender/llgl.h
@@ -137,9 +137,6 @@ public:
 	// Misc extensions
 	BOOL mHasSeparateSpecularColor;
 
-	//whether this GPU is in the debug list.
-	BOOL mDebugGPU;
-	
 	S32 mDriverVersionMajor;
 	S32 mDriverVersionMinor;
 	S32 mDriverVersionRelease;
@@ -171,7 +168,6 @@ private:
 	void initExtensions();
 	void initGLStates();
 	void initGLImages();
-	void setToDebugGPU();
 };
 
 extern LLGLManager gGLManager;
