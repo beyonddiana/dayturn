2b684a415250a0d7d82b5f876454321efdc0e84b
Geir Nøklebye <geir.noklebye@dayturn.com>
2020-10-20 21:23:45 +0200

    Update to llkeyframemotion after DAYTW-32 Convert BOOL in lldatapacker

--- indra/llcharacter/llkeyframemotion.cpp
+++ indra/llcharacter/llkeyframemotion.cpp
@@ -1555,7 +1555,7 @@ BOOL LLKeyframeMotion::deserialize(LLDataPacker& dp, const LLUUID& asset_id)
 			LLVector3 rot_angles;
 			U16 x, y, z;
 
-			BOOL success = TRUE;
+			bool success = true;
 
 			if (old_version)
 			{
@@ -1581,7 +1581,7 @@ BOOL LLKeyframeMotion::deserialize(LLDataPacker& dp, const LLUUID& asset_id)
 			{
 				LL_WARNS() << "non-finite angle in rotation key"
                            << " for animation " << asset_id << LL_ENDL;
-				success = FALSE;
+				success = false;
 			}
 			
 			if (!success)
@@ -1642,7 +1642,7 @@ BOOL LLKeyframeMotion::deserialize(LLDataPacker& dp, const LLUUID& asset_id)
 				pos_key.mTime = U16_to_F32(time_short, 0.f, mJointMotionList->mDuration);
 			}
 
-			BOOL success = TRUE;
+			bool success = true;
 
 			if (old_version)
 			{
@@ -1671,7 +1671,7 @@ BOOL LLKeyframeMotion::deserialize(LLDataPacker& dp, const LLUUID& asset_id)
 			{
 				LL_WARNS() << "non-finite position in key"
                            << " for animation " << asset_id << LL_ENDL;
-				success = FALSE;
+				success = false;
 			}
 			
 			if (!success)
@@ -1953,7 +1953,7 @@ BOOL LLKeyframeMotion::deserialize(LLDataPacker& dp, const LLUUID& asset_id)
 //-----------------------------------------------------------------------------
 BOOL LLKeyframeMotion::serialize(LLDataPacker& dp) const
 {
-	BOOL success = TRUE;
+	bool success = true;
 
 	LL_DEBUGS("BVH") << "serializing" << LL_ENDL;
 
