e6fefa529fda802771101dcf7c77813868f65d1d
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2020-10-29 16:10:40 +0200

    SL-14226 Cleanup teleport requests

--- indra/newview/llagent.cpp
+++ indra/newview/llagent.cpp
@@ -489,6 +489,8 @@ void LLAgent::init()
 void LLAgent::cleanup()
 {
 	mRegionp = NULL;
+    mTeleportRequest = NULL;
+    mTeleportCanceled = NULL;
 	if (mTeleportFinishedSlot.connected())
 	{
 		mTeleportFinishedSlot.disconnect();
