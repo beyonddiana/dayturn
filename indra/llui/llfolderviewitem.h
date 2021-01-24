5f9192bbb23b9211d30908426662ac275df8b232
Geir Nøklebye <geir.noklebye@dayturn.com>
2021-01-24 20:47:59 +0100

    DAYTW-32 handleDoubleClick is a BOOL for now

--- indra/llui/llfolderviewitem.h
+++ indra/llui/llfolderviewitem.h
@@ -428,7 +428,7 @@ public:
 	virtual BOOL handleRightMouseDown( S32 x, S32 y, MASK mask );
 	virtual BOOL handleMouseDown( S32 x, S32 y, MASK mask );
 	virtual BOOL handleDoubleClick( S32 x, S32 y, MASK mask );
-	virtual bool handleDoubleClick( S32 x, S32 y, MASK mask );
+	virtual BOOL handleDoubleClick( S32 x, S32 y, MASK mask );
 	virtual bool handleDragAndDrop(S32 x, S32 y, MASK mask,
 									bool drop,
 									EDragAndDropType cargo_type,
