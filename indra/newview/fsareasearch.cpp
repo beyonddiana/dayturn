00c1dfc7312e5784457cfb9180afaa5766095325
Geir Nøklebye <geir.noklebye@dayturn.com>
2021-02-19 22:24:39 +0100

    Update information and license info for fsareasearch + cosmetics
    # Conflicts:
    #	indra/newview/fsareasearch.cpp
    #	indra/newview/fsareasearch.h

--- indra/newview/fsareasearch.cpp
+++ indra/newview/fsareasearch.cpp
@@ -1,66 +1,29 @@
-/** 
+/**
  * @file fsareasearch.cpp
- * @brief Search the local area for objects.
+ * @brief Floater to search and list objects in view or is known to the viewer.
  *
- * Copyright (c) 2013 Jessica Wabbit
- * Portions copyright (c) 2013 Linden Research, Inc.
- * Portions copyright (c) 2013 Various Firestorm viewer contributors
- * Portions copyright (c) 2009 Modular Systems Ltd
+ * $LicenseInfo:firstyear=2012&license=viewerlgpl$
+ * Phoenix Firestorm Viewer Source Code
+ * Copyright (c) 2012 Techwolf Lupindo
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation;
  * version 2.1 of the License only.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * ----------------------------------------------------------------------------
- *
- * This source code is licenced under the terms of the LGPL version 2.1,
- * detailed above.  This file contains portions of source code provided
- * under a BSD-style licence, and the following text is included to comply
- * with the terms of that original licence:
- *
- * Copyright (c) 2009
- *
- * Modular Systems Ltd. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following
- * conditions are met IN ADDITION TO THE TERMS OF THE LGPL v2.1:
- *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above
- *      copyright notice, this list of conditions and the following
- *      disclaimer in the documentation and/or other materials provided
- *      with the distribution.
- *   3. Neither the name Modular Systems Ltd nor the names of its contributors
- *      may be used to endorse or promote products derived from this
- *      software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY MODULAR SYSTEMS LTD AND CONTRIBUTORS “AS IS”
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MODULAR SYSTEMS OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Modified, debugged, optimized and improved by Henri Beauchamp Feb 2010.
- * Refactored for Viewer2 by Kadah Coba, April 2011
- * Rewritten for viewer 3 by Jessica Wabbit, November 2013
+ * The Phoenix Firestorm Project, Inc., 1831 Oakwood Drive, Fairmont, Minnesota 56031-3225 USA
+ * http://www.firestormviewer.org
+ * $/LicenseInfo$
  */
 
 #include "llviewerprecompiledheaders.h"
@@ -100,7 +63,7 @@
 class FSAreaSearch::FSParcelChangeObserver : public LLParcelObserver
 {
 public:
-	FSParcelChangeObserver(FSAreaSearch *area_search_floater) : mAreaSearchFloater(area_search_floater) {}
+	FSParcelChangeObserver(FSAreaSearch* area_search_floater) : mAreaSearchFloater(area_search_floater) {}
 
 private:
 	/*virtual*/ void changed()
