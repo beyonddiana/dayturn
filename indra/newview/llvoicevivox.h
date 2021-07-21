4e63a7838fab3d463a153236de8e2581a1a135ba
Geir Nøklebye <geir.noklebye@dayturn.com>
2021-07-21 12:12:47 +0200

    Lifted from SL-15072: First part of adding ability to disable the VIVOX auto VAD feature and set our own values via Debug Settings - hardcoded currently for testing but will be pulled from global settings eventually

--- indra/newview/llvoicevivox.h
+++ indra/newview/llvoicevivox.h
@@ -470,7 +470,6 @@ protected:
 	// local audio updates, mic mute, speaker mute, mic volume and speaker volumes
 	void sendLocalAudioUpdates();		
 
-
 	/////////////////////////////
 	// Response/Event handlers
 	void connectorCreateResponse(int statusCode, std::string &statusString, std::string &connectorHandle, std::string &versionID);
@@ -495,6 +494,12 @@ protected:
 	void sessionNotificationEvent(std::string &sessionHandle, std::string &uriString, std::string &notificationType);
 	
 	void muteListChanged();
+
+	/////////////////////////////
+	// VAD changes
+	// disable auto-VAD and configure VAD parameters explicitly
+	void setupVADParams(unsigned int vad_hangover, unsigned int vad_noise_floor, unsigned int vad_sensitivity);
+	void onVADSettingsChange();
 		
 	/////////////////////////////
 	// Sending updates of current state
