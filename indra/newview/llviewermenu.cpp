9125410611c2f365abad94b31861be12085a885c
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2021-09-28 19:43:24 +0300

    SL-16098 Unable to sit on an object using the 'Object inspector' floater
    The same should probably be done for onClickTouch

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -4251,21 +4251,8 @@ bool is_object_sittable()
 
 
 // only works on pie menu
-void handle_object_sit_or_stand()
+void handle_object_sit(LLViewerObject *object, const LLVector3 &offset)
 {
-	LLPickInfo pick = LLToolPie::getInstance()->getPick();
-	LLViewerObject *object = pick.getObject();;
-	if (!object || pick.mPickType == LLPickInfo::PICK_FLORA)
-	{
-		return;
-	}
-
-	if (sitting_on_selection())
-	{
-		gAgent.standUp();
-		return;
-	}
-
 	// get object selection offset 
 
 	if (object && object->getPCode() == LL_PCODE_VOLUME)
@@ -4277,7 +4264,7 @@ void handle_object_sit_or_stand()
 		gMessageSystem->addUUIDFast(_PREHASH_SessionID, gAgent.getSessionID());
 		gMessageSystem->nextBlockFast(_PREHASH_TargetObject);
 		gMessageSystem->addUUIDFast(_PREHASH_TargetID, object->mID);
-		gMessageSystem->addVector3Fast(_PREHASH_Offset, pick.mObjectOffset);
+		gMessageSystem->addVector3Fast(_PREHASH_Offset, offset);
 
 		object->getRegion()->sendReliableMessage();
 	}
@@ -4297,6 +4284,36 @@ void handle_object_teleport()
 		pos.mdV[VZ] += gAgentAvatarp->getPelvisToFoot();
 		gAgent.teleportViaLocationLookAt(pos);
 	}
+}
+
+void handle_object_sit_or_stand()
+{
+    LLPickInfo pick = LLToolPie::getInstance()->getPick();
+    LLViewerObject *object = pick.getObject();
+    if (!object || pick.mPickType == LLPickInfo::PICK_FLORA)
+    {
+        return;
+    }
+
+    if (sitting_on_selection())
+    {
+        gAgent.standUp();
+        return;
+    }
+
+    handle_object_sit(object, pick.mObjectOffset);
+}
+
+void handle_object_sit(const LLUUID& object_id)
+{
+    LLViewerObject* obj = gObjectList.findObject(object_id);
+    if (!obj)
+    {
+        return;
+    }
+
+    LLVector3 offset(0, 0, 0);
+    handle_object_sit(obj, offset);
 }
 
 void near_sit_down_point(bool success, void *)
