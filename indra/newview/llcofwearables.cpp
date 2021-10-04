secondlife/viewer

BUG-228664: Add an 'Edit' option for attachments to the various context menus

indra/newview/llcofwearables.cpp
@@ -140,9 +140,25 @@ protected:
 	{
 		LLUICtrl::CommitCallbackRegistry::ScopedRegistrar registrar;
 
+		registrar.add("Attachment.Edit", boost::bind(handleMultiple, handle_item_edit, mUUIDs));
 		registrar.add("Attachment.Detach", boost::bind(&LLAppearanceMgr::removeItemsFromAvatar, LLAppearanceMgr::getInstance(), mUUIDs));
 
+		LLUICtrl::EnableCallbackRegistry::ScopedRegistrar enable_registrar;
+		enable_registrar.add("Attachment.OnEnable", boost::bind(&CofAttachmentContextMenu::onEnable, this, _2));
+
 		return createFromFile("menu_cof_attachment.xml");
+	}
+
+	bool onEnable(const LLSD& userdata)
+	{
+		const std::string event_name = userdata.asString();
+
+		if ("edit" == event_name)
+		{
+			return (1 == mUUIDs.size()) && (get_is_item_editable(mUUIDs.front()));
+		}
+
+		return true;
 	}
 };
 

beyonddiana/dayturn

indra/newview/llcofwearables.cpp
@@ -140,11 +140,11 @@ protected:
 	{
 		LLUICtrl::CommitCallbackRegistry::ScopedRegistrar registrar;
 
+		registrar.add("Attachment.Edit", boost::bind(handleMultiple, handle_item_edit, mUUIDs));
 		registrar.add("Attachment.Detach", boost::bind(&LLAppearanceMgr::removeItemsFromAvatar, LLAppearanceMgr::getInstance(), mUUIDs));
 // [SL:KB] - Patch: Inventory-AttachmentActions - Checked: 2012-05-15 (Catznip-3.3)
 		registrar.add("Attachment.Touch", boost::bind(handleMultiple, handle_attachment_touch, mUUIDs));
 // [/SL:KB]
-		registrar.add("Attachment.Edit", boost::bind(handleMultiple, handle_item_edit, mUUIDs));
 
 		LLUICtrl::EnableCallbackRegistry::ScopedRegistrar enable_registrar;
 		enable_registrar.add("Attachment.OnEnable", boost::bind(&CofAttachmentContextMenu::onEnable, this, _2));
@@ -152,16 +152,17 @@ protected:
 		return createFromFile("menu_cof_attachment.xml");
 	}
 
-	bool onEnable(const LLSD& sdParam)
+	bool onEnable(const LLSD& userdata)
 	{
-		std::string strParam = sdParam.asString();
-		if ("touch_attach" == strParam)
+	    const std::string event_name = userdata.asString();
+
+		if ("touch_attach" == event_name)
 		{
 			return (1 == mUUIDs.size()) && (enable_attachment_touch(mUUIDs.front()));
 		}
-		else if ("edit_item" == strParam)
+		else if ("edit" == event_name)
 		{
-			return (1 == mUUIDs.size()) && (enable_item_edit(mUUIDs.front()));
+			return (1 == mUUIDs.size()) && (get_is_item_editable(mUUIDs.front()));
 		}
 		return true;
 	}