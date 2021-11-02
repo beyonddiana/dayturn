4d89e20afe0496ca5dc40b7fdddc3e77b54819ac
Geir Nøklebye <geir.noklebye@dayturn.com>
2021-11-02 20:50:42 +0100

    Only direct to the listing of release notes for all versions from the Release Notes link in the viewer About Dayturn box.

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -3253,9 +3253,11 @@ LLSD LLAppViewer::getViewerInfo() const
 	// return a URL to the release notes for this viewer, such as:
 	// http://wiki.secondlife.com/wiki/Release_Notes/Second Life Beta Viewer/2.1.0.123456
 	std::string url = LLTrans::getString("RELEASE_NOTES_BASE_URL");
+/* 
 	if (! LLStringUtil::endsWith(url, "/"))
 		url += "/";
     url += LLURI::escape(LLVersionInfo::instance().getVersion()) + ".html";
+ */
 //	std::string channel = LLVersionInfo::getChannel();
 // 	if (LLStringUtil::endsWith(boost::to_lower_copy(channel), " edu")) // Release Notes url shouldn't include the EDU parameter
 // 	{
