c107e3887dc570de78581cbc580e5f23509b8c8c
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2021-11-06 00:37:54 +0200

    SL-13867 Developer ability to select and reposition multiple avatars

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -5374,7 +5374,7 @@ void LLAppViewer::disconnectViewer()
 		gFloaterView->restoreAll();
 	}
 
-	if (LLSelectMgr::getInstance())
+	if (LLSelectMgr::instanceExists())
 	{
 		LLSelectMgr::getInstance()->deselectAll();
 	}
