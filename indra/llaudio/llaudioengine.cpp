1910ea267217a6a206987e08fb4bf6084d2b600f
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2021-11-26 22:56:20 +0200

    SL-16339 Rename setAmbient to not cause confusion
    setAmbient has nothing to do with ambient sounds

--- indra/llaudio/llaudioengine.cpp
+++ indra/llaudio/llaudioengine.cpp
@@ -834,7 +834,8 @@ void LLAudioEngine::triggerSound(const LLUUID &audio_uuid, const LLUUID& owner_i
 	addAudioSource(asp);
 	if (pos_global.isExactlyZero())
 	{
-		asp->setAmbient(true);
+		// For sound preview and UI
+		asp->setForcedPriority(true);
 	}
 	else
 	{
@@ -1281,7 +1282,7 @@ LLAudioSource::LLAudioSource(const LLUUID& id, const LLUUID& owner_id, const F32
 	mPriority(0.f),
 	mGain(gain),
 	mSourceMuted(false),
-	mAmbient(false),
+	mForcedPriority(false),
 	mLoop(false),
 	mSyncMaster(false),
 	mSyncSlave(false),
@@ -1418,7 +1419,7 @@ void LLAudioSource::update()
 
 void LLAudioSource::updatePriority()
 {
-	if (isAmbient())
+	if (isForcedPriority())
 	{
 		mPriority = 1.f;
 	}
