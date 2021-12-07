2f37257406f4036c6c5f15b84129ff739653e83b
Geir Nøklebye <geir.noklebye@dayturn.com>
2021-12-07 16:32:33 +0100

    Add everything but upload_bulk from MAINT-8700 FIXED The File dialog disconnects the viewer if you do not choose upload files within the first minute. Upload_bulk needs has a dependency on llassetupload, so needs more work
    # Conflicts:
    #	indra/newview/llpreviewscript.cpp
    #	indra/newview/llviewermenufile.cpp
    #	indra/newview/llviewermenufile.h

--- indra/newview/llpreviewscript.cpp
+++ indra/newview/llpreviewscript.cpp
@@ -62,7 +62,7 @@
 #include "llselectmgr.h"
 #include "llviewerinventory.h"
 #include "llviewermenu.h"
-#include "llviewerobject.h"
+#include "llviewermenufile.h" // LLFilePickerReplyThread
 #include "llviewerobjectlist.h"
 #include "llviewerregion.h"
 #include "llkeyboard.h"
@@ -1229,17 +1229,12 @@ bool LLScriptEdCore::handleKeyHere(KEY key, MASK mask)
 
 void LLScriptEdCore::onBtnLoadFromFile( void* data )
 {
-	LLScriptEdCore* self = (LLScriptEdCore*) data;
+	(new LLFilePickerReplyThread(boost::bind(&LLScriptEdCore::loadScriptFromFile, _1, data), LLFilePicker::FFLOAD_SCRIPT, false))->getFile();
+}
 
-	// TODO Maybe add a dialogue warning here if the current file has unsaved changes.
-	LLFilePicker& file_picker = LLFilePicker::instance();
-	if( !file_picker.getOpenFile( LLFilePicker::FFLOAD_SCRIPT ) )
-	{
-		//File picking cancelled by user, so nothing to do.
-		return;
-	}
-
-	std::string filename = file_picker.getFirstFile();
+void LLScriptEdCore::loadScriptFromFile(const std::vector<std::string>& filenames, void* data)
+{
+	std::string filename = filenames[0];
 
 	llifstream fin(filename.c_str());
 
@@ -1247,8 +1242,8 @@ void LLScriptEdCore::onBtnLoadFromFile( void* data )
 	std::string text;
 	std::string linetotal;
 	while (!fin.eof())
-	{ 
-		getline(fin,line);
+	{
+		getline(fin, line);
 		text += line;
 		if (!fin.eof())
 		{
@@ -1258,7 +1253,8 @@ void LLScriptEdCore::onBtnLoadFromFile( void* data )
 	fin.close();
 
 	// Only replace the script if there is something to replace with.
-	if (text.length() > 0)
+	LLScriptEdCore* self = (LLScriptEdCore*)data;
+	if (self && (text.length() > 0))
 	{
 		self->mEditor->selectAll();
 		LLWString script(utf8str_to_wstring(text));
@@ -1274,16 +1270,21 @@ void LLScriptEdCore::onBtnSaveToFile( void* userdata )
 
 	if( self->mSaveCallback )
 	{
-		LLFilePicker& file_picker = LLFilePicker::instance();
-		if( file_picker.getSaveFile( LLFilePicker::FFSAVE_SCRIPT, self->mScriptName ) )
-		{
-			std::string filename = file_picker.getFirstFile();
-			std::string scriptText=self->mEditor->getText();
-			llofstream fout(filename.c_str());
-			fout<<(scriptText);
-			fout.close();
-			self->mSaveCallback( self->mUserdata, FALSE );
-		}
+		(new LLFilePickerReplyThread(boost::bind(&LLScriptEdCore::saveScriptToFile, _1, userdata), LLFilePicker::FFSAVE_SCRIPT, self->mScriptName))->getFile();
+	}
+}
+
+void LLScriptEdCore::saveScriptToFile(const std::vector<std::string>& filenames, void* data)
+{
+	LLScriptEdCore* self = (LLScriptEdCore*)data;
+	if (self)
+	{
+		std::string filename = filenames[0];
+		std::string scriptText = self->mEditor->getText();
+		llofstream fout(filename.c_str());
+		fout << (scriptText);
+		fout.close();
+		self->mSaveCallback(self->mUserdata, FALSE);
 	}
 }
 
