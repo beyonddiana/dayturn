d98d357e8ab2e34ac6945b4715667788f0fbf02e
Andrey Kleshchev <andreykproductengine@lindenlab.com>
2021-12-07 17:56:02 +0200

    SL-16450 Don not crash viewer in case of invalid data type

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -1420,6 +1420,10 @@ bool idle_startup()
 
 						}
 					}
+                    else if (reason_response == "BadType")
+                    {
+                        LLNotificationsUtil::add("LoginFailedToParse", LLSD(), LLSD(), login_alert_done);
+                    }
 					else 
 					{
 						// This wasn't a certificate error, so throw up the normal
